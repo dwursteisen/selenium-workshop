6518ba07b81a3ab2af327d6ef946f2f790cf112a
David Wursteisen <david.wursteisen+github@gmail.com>
2014-07-04 15:02:48 +0200

    ajout de dotclear

--- Vagrantfile
+++ Vagrantfile
@@ -34,6 +34,10 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # Default value: false
   # config.ssh.forward_agent = true
 
+  
+  config.ssh.username = "formation"
+  # config.ssh.password = "formation"
+  
   # Share an additional folder to the guest VM. The first argument is
   # the path on the host to the actual folder. The second argument is
   # the path on the guest to mount the folder. And the optional third
--- cookbooks/dotclear/recipes/default.rb
+++ cookbooks/dotclear/recipes/default.rb
@@ -5,3 +5,45 @@ end
 service 'apache2' do
   action [ :enable, :start ]
 end
+
+package 'php5'
+package 'libapache2-mod-php5'
+package 'mysql-server'
+package 'php5-mysql'
+
+cookbook_file 'index.php' do
+	path "/var/www/index.php"
+	action :create
+end
+
+dotclear_latest = "#{Chef::Config[:file_cache_path]}/dotclear.tar.gz"
+
+cookbook_file "dotclear-2.6.3.tar.gz" do
+  path dotclear_latest
+  mode "0644"
+end
+
+execute "untar-dotclear" do
+	cwd "/var/www/"
+	command "tar --strip-components 1 -xzf " + dotclear_latest
+end
+
+directory "/var/www/dotclear/cache" do
+	mode "0776"
+end
+
+directory "/var/www/dotclear/public" do
+	mode "0776"
+end
+
+execute "create-dotclear-database" do
+	command "mysqladmin -uroot create dotclear"
+	not_if { FileTest.exist?("/var/www/dotclear/inc/config.php") }
+end
+
+cookbook_file 'config.php' do
+	path "/var/www/dotclear/inc/config.php"
+	action :create_if_missing
+end
+
+
