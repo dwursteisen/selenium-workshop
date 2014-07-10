18fe4df567683f3dc62e4d815c9022e9cd4c6d4b
David Wursteisen <david.wursteisen+github@gmail.com>
2014-07-10 16:24:28 +0200

    ajout de javascript pour dynamiser le formulaire

--- cookbooks/workshop/recipes/default.rb
+++ cookbooks/workshop/recipes/default.rb
@@ -7,3 +7,9 @@ cookbook_file 'bootstrap.min.css' do
 	path "/var/www/bootstrap.min.css"
 	action :create
 end
+
+
+cookbook_file 'jquery-2.1.1.min.js' do
+	path "/var/www/jquery-2.1.1.min.js"
+	action :create
+end
