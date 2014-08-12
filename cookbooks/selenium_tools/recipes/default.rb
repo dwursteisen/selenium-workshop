6b5e3e9f2a59c5d90212b31043c5bce0bdb9c9e5
David Wursteisen <david.wursteisen+github@gmail.com>
2014-08-12 10:25:46 +0200

    force le telechargement de fichier dans le cas où il y a corruption de fichier

--- cookbooks/selenium_tools/recipes/default.rb
+++ cookbooks/selenium_tools/recipes/default.rb
@@ -2,7 +2,7 @@
 
 remote_file "/home/formation/phantomjs.tar.bz2" do
   source "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2"
-  action :create_if_missing
+  action :create
   not_if { ::File.directory?("/home/formation/phantomjs-1.9.7-linux-x86_64")}
 end
 
@@ -14,7 +14,7 @@ end
 
 remote_file "/home/formation/selenium-server.jar" do
   source "http://selenium-release.storage.googleapis.com/2.42/selenium-server-standalone-2.42.2.jar"
-  action :create_if_missing
+  action :create
   not_if { ::File.directory?("/home/formation/selenium-server.jar")}
 end
 
